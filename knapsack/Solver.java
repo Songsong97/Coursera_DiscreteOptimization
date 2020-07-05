959e454db8b72e5d5a64f535a7fe0d917f1ad27a
fakeljl <694118974@qq.com>
2020-07-05 15:23:27 -0400

    Add dynamic programming to knapsack

--- knapsack/Solver.java
+++ knapsack/Solver.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.lang.Math;
 
 /**
  * The class <code>Solver</code> is an implementation of a greedy algorithm to solve the knapsack problem.
@@ -58,34 +59,52 @@ public class Solver {
         int[] weights = new int[items];
 
         for(int i=1; i < items+1; i++){
-          String line = lines.get(i);
-          String[] parts = line.split("\\s+");
+            String line = lines.get(i);
+            String[] parts = line.split("\\s+");
 
-          values[i-1] = Integer.parseInt(parts[0]);
-          weights[i-1] = Integer.parseInt(parts[1]);
+            values[i-1] = Integer.parseInt(parts[0]);
+            weights[i-1] = Integer.parseInt(parts[1]);
         }
 
-        // a trivial greedy algorithm for filling the knapsack
-        // it takes items in-order until the knapsack is full
-        int value = 0;
-        int weight = 0;
+        // Dynamic Programming
+        int[][] dp = new int[items][capacity + 1];
         int[] taken = new int[items];
+        int value = 0;
 
-        for(int i=0; i < items; i++){
-            if(weight + weights[i] <= capacity){
-                taken[i] = 1;
-                value += values[i];
-                weight += weights[i];
-            } else {
-                taken[i] = 0;
+        for (int i = 0; i < items; i++) {
+            for (int j = 0; j <= capacity; j++) {
+                if (i == 0) {
+                    dp[i][j] = weights[i] <= j ? values[i] : 0;
+                }
+                else if (weights[i] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i]);
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
-        
+
+        for (int i = items - 1, k = capacity; i >= 0; i--) {
+            if (i == 0) {
+                taken[i] = dp[i][k] == 0 ? 0 : 1;
+            }
+            else if (dp[i][k] == dp[i - 1][k]) {
+                taken[i] = 0;
+            }
+            else {
+                k -= weights[i];
+                taken[i] = 1;
+            }
+        }
+
+        value = dp[items - 1][capacity];
+
         // prepare the solution in the specified output format
-        System.out.println(value+" 0");
+        System.out.println(value+" 1");
         for(int i=0; i < items; i++){
             System.out.print(taken[i]+" ");
         }
-        System.out.println("");        
+        System.out.println("");
     }
 }

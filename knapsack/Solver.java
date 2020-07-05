592de3888213c26f884c3c56edab661613fd9f58
fakeljl <694118974@qq.com>
2020-07-05 19:54:50 -0400

    Add recursive search

--- knapsack/Solver.java
+++ knapsack/Solver.java
@@ -1,17 +1,19 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.Math;
 
-/**
- * The class <code>Solver</code> is an implementation of a greedy algorithm to solve the knapsack problem.
- *
- */
 public class Solver {
-    
-    /**
-     * The main class
-     */
+    static int maxValue = 0;
+    static int items;
+    static int capacity;
+    static int[] values;
+    static int[] weights;
+    static int[] taken;
+    static int[] curTaken;
+
     public static void main(String[] args) {
         try {
             solve(args);
@@ -19,7 +21,126 @@ public class Solver {
             e.printStackTrace();
         }
     }
-    
+
+    /**
+     * Dynamic Programming
+     */
+    public static void solveDP() {
+        int[][] dp = new int[items][capacity + 1];
+        taken = new int[items];
+
+        for (int i = 0; i < items; i++) {
+            for (int j = 0; j <= capacity; j++) {
+                if (i == 0) {
+                    dp[i][j] = weights[i] <= j ? values[i] : 0;
+                }
+                else if (weights[i] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i]);
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        for (int i = items - 1, k = capacity; i >= 0; i--) {
+            if (i == 0) {
+                taken[i] = dp[i][k] == 0 ? 0 : 1;
+            }
+            else if (dp[i][k] == dp[i - 1][k]) {
+                taken[i] = 0;
+            }
+            else {
+                k -= weights[i];
+                taken[i] = 1;
+            }
+        }
+
+        // prepare the solution in the specified output format
+        System.out.println(dp[items - 1][capacity]+" 1");
+        for(int i=0; i < items; i++){
+            System.out.print(taken[i]+" ");
+        }
+        System.out.println("");
+    }
+
+    public static void solve2() {
+        int[][] temp = new int[items][3];
+        for (int i = 0; i < items; i++) {
+            temp[i][0] = values[i];
+            temp[i][1] = weights[i];
+            temp[i][2] = i;
+        }
+        Arrays.sort(temp, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                float t = (float)o2[0] / (float)o2[1] - (float)o1[0] / (float)o1[1];
+                return t == 0 ? 0 : (t > 0 ? 1 : -1);
+            }
+        });
+        for (int i = 0; i < items; i++) {
+            values[i] = temp[i][0];
+            weights[i] = temp[i][1];
+        }
+        curTaken = new int[items];
+        int greedySpace = capacity;
+        float bounding = 0;
+        for (int i = 0; i < items; i++) {
+            if (weights[i] <= greedySpace) {
+                greedySpace -= weights[i];
+                bounding += values[i];
+            }
+            else {
+                bounding += greedySpace / (float)weights[i] * (float)values[i];
+                break;
+            }
+        }
+        helper(0, 0, capacity, bounding);
+        int[] initTaken = new int[items];
+        for (int i = 0; i < items; i++) {
+            initTaken[temp[i][2]] = taken[i];
+        }
+
+        // prepare the solution in the specified output format
+        System.out.println(maxValue+" 1");
+        for(int i=0; i < items; i++){
+            System.out.print(initTaken[i]+" ");
+        }
+        System.out.println("");
+    }
+
+    public static void helper(int i, int value, int room, float bounding) {
+        if (bounding <= maxValue) {
+            return;
+        }
+        if (room < 0) {
+            return;
+        }
+        if (i == items) {
+            if (maxValue < value) {
+                maxValue = value;
+                taken = curTaken.clone();
+            }
+            return;
+        }
+        curTaken[i] = 1;
+        helper(i + 1, value + values[i], room - weights[i], bounding);
+        curTaken[i] = 0;
+        bounding = value;
+        int greedySpace = room;
+        for (int j = i + 1; j < items; j++) {
+            if (weights[j] <= greedySpace) {
+                greedySpace -= weights[j];
+                bounding += values[j];
+            }
+            else {
+                bounding += greedySpace / (float)weights[j] * (float)values[j];
+                break;
+            }
+        }
+        helper(i + 1, value, room, bounding);
+    }
+
     /**
      * Read the instance, solve it, and print the solution in the standard output
      */
@@ -49,14 +170,13 @@ public class Solver {
             input.close();
         }
         
-        
         // parse the data in the file
         String[] firstLine = lines.get(0).split("\\s+");
-        int items = Integer.parseInt(firstLine[0]);
-        int capacity = Integer.parseInt(firstLine[1]);
+        items = Integer.parseInt(firstLine[0]);
+        capacity = Integer.parseInt(firstLine[1]);
 
-        int[] values = new int[items];
-        int[] weights = new int[items];
+        values = new int[items];
+        weights = new int[items];
 
         for(int i=1; i < items+1; i++){
             String line = lines.get(i);
@@ -66,45 +186,6 @@ public class Solver {
             weights[i-1] = Integer.parseInt(parts[1]);
         }
 
-        // Dynamic Programming
-        int[][] dp = new int[items][capacity + 1];
-        int[] taken = new int[items];
-        int value = 0;
-
-        for (int i = 0; i < items; i++) {
-            for (int j = 0; j <= capacity; j++) {
-                if (i == 0) {
-                    dp[i][j] = weights[i] <= j ? values[i] : 0;
-                }
-                else if (weights[i] <= j) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i]);
-                }
-                else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-
-        for (int i = items - 1, k = capacity; i >= 0; i--) {
-            if (i == 0) {
-                taken[i] = dp[i][k] == 0 ? 0 : 1;
-            }
-            else if (dp[i][k] == dp[i - 1][k]) {
-                taken[i] = 0;
-            }
-            else {
-                k -= weights[i];
-                taken[i] = 1;
-            }
-        }
-
-        value = dp[items - 1][capacity];
-
-        // prepare the solution in the specified output format
-        System.out.println(value+" 1");
-        for(int i=0; i < items; i++){
-            System.out.print(taken[i]+" ");
-        }
-        System.out.println("");
+        solve2();
     }
 }
